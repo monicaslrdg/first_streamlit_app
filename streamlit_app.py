3f9c9340cc30e26208f1cdc495a1b0cda1509913
monicaslrdg <133094096+monicaslrdg@users.noreply.github.com>
2023-05-10 16:14:00 +0200

    try else error

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fuityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalizing the json data with panda 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Create  anew table with the watermelon info
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 #This means anithing will be running from here above
 streamlit.stop()
 
